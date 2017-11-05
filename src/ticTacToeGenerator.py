125d849817c1b7430a91c9b90266774bcebfc82d
joshuajcarson <joshuajcarson@gmail.com>
2017-11-05 15:23:17 -0600

    Cleaned up project to use PyCharm

--- src/ticTacToeGenerator.py
+++ src/ticTacToeGenerator.py
@@ -1,29 +1,24 @@
-"""
-Created on Sun Nov  5 05:43:08 2017
-
-@author: joshuajcarson
-"""
-
 import numpy as np
 import pandas as pd
 
+
 class gameBoard(object):
     def __init__(self):
-        self.data = []        
+        self.data = []
         self.stateColumn = 'state'
         self.playerOneMark = 1
         self.playerTwoMark = -1
         self.board = self.createEmptyBoard()
 
     def createEmptyBoard(self):
-        return pd.DataFrame({self.stateColumn : np.zeros((9,))})
+        return pd.DataFrame({self.stateColumn: np.zeros((9,))})
 
     def makeMove(self, spot, player):
         self.board.at[spot, self.stateColumn] = player
-        
+
     def possibleMoves(self):
         return self.board.query(str(self.stateColumn, " == 0"))
-    
+
     def didPlayerOneWin(self):
         if sum(self.board[0:3]['state']) == 3:
             return True
